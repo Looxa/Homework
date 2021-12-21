ac130ed755ad3590c5c8b70694099eaf0db728f8
Looxa <imxoloxas@gmail.com>
2021-12-21 12:14:52 +0300

    BugFix

--- Program.cs
+++ Program.cs
@@ -15,25 +15,28 @@ namespace Homework_1_v._1
             switch (DayOfWeek)
             {
                 case 1:
-                Console.WriteLine("Сегодня понедельник");
+                    Console.WriteLine("Сегодня понедельник");
                     break;
                 case 2:
-                Console.WriteLine("Сегодня вторник");
+                    Console.WriteLine("Сегодня вторник");
                     break;
                 case 3:
-                Console.WriteLine("Сегодня среда");
+                    Console.WriteLine("Сегодня среда");
                     break;
                 case 4:
-                Console.WriteLine("Сегодня четверг");
+                    Console.WriteLine("Сегодня четверг");
                     break;
                 case 5:
-                Console.WriteLine("Сегодня пятница");
+                    Console.WriteLine("Сегодня пятница");
                     break;
                 case 6:
-                Console.WriteLine("Сегодня суббота");
+                    Console.WriteLine("Сегодня суббота");
+                    break;
+                case 7:
+                    Console.WriteLine("Сегодня воскресенье");
                     break;
                 default:
-                Console.WriteLine("Сегодня воскресенье");
+                    Console.WriteLine("Неверное число");
                     break;
             }
             Console.ReadKey(); // Сообщение для проверки работы git
