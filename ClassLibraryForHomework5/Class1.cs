aa14a917a18e4c22d5767bc484a9b324e2dd95d0
Looxa <imxoloxas@gmail.com>
2022-01-20 19:07:09 +0300

    Добавлено начало задачи Homework 5

---
+++ ClassLibraryForHomework5/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForHomework5
+{
+    public class Class1
+    {
+        public string name = "Название продукта";
+        public double price;
+        public int id;
+    }
+}
---
+++ Homework 5/Homework 5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryForHomework5;
+
+namespace Homework_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
