dc71e01138fba7e86851d1ca1b85952ad3b32d63
Looxa <imxoloxas@gmail.com>
2022-01-21 01:27:26 +0300

    Не реализовал удаление

---
+++ ClassLibraryForHomework5/Items.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForHomework5
+{
+    public class Items // Класс продуктов
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public int id { get; set; }
+    }
+
+
+    public class Inventory // Класс инвентаря 
+    {
+
+        List<Items> listOfItems = new List<Items>();
+        Items item1 = new Items();
+        public void Add(int id1, string name1, double price1) // Метод добавления продукта
+        {
+            Items itemX = new Items() { id = id1, name = name1, price = price1 };
+            listOfItems.Add(itemX);
+        }
+        public void Delete(int n)  // Метод удаления продукта
+        {
+            item1.id = n;
+            listOfItems.RemoveAt(listOfItems.IndexOf(item1));
+        }
+        public void List()  //Метод вывода списка продуктов
+        {
+            double PriceCount = 0;
+            for (int i = 0; i < listOfItems.Count; i++)
+            {
+                Console.WriteLine("Id: " + listOfItems[i].id + "\n Имя продукта: " + listOfItems[i].name + "\n Цена: " + listOfItems[i].price);
+                if (listOfItems[i].price == 0)
+                {
+                    PriceCount = listOfItems[i].price;
+                }
+                else
+                {
+                    PriceCount = listOfItems[i].price + listOfItems[i - 1].price;
+                }
+            }
+            Console.WriteLine("Цена всего инвентаря: " + PriceCount);
+        }
+    }
+
+}
--- Homework 5/Homework 5/Program.cs
+++ Homework 5/Homework 5/Program.cs
@@ -11,6 +11,65 @@ namespace Homework_5
     {
         static void Main(string[] args)
         {
+            Items item = new Items();
+            Inventory list = new Inventory();
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие : \nЦифра 1 - Список продуктов" +
+                "\nЦифра 2 - Добавить продукт" +
+                " \nЦифра 3 - Удалить продукт" + 
+                "\nЦифра 4 - Выйти");
+                int SwitchAction = Convert.ToInt32(Console.ReadLine());
+                if (SwitchAction == 4) break;
+
+                switch (SwitchAction)
+                {
+                    case 1:
+
+                        list.List();
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine("Введите наименование продукта");
+                        item.name = Console.ReadLine();
+                        Console.WriteLine("Введите цену продукта");
+                        string inputPrice = Console.ReadLine();
+                        bool resultTryParse = double.TryParse(inputPrice, out var ReadPrice);
+                        if (resultTryParse == true)
+                        {
+                            item.price = ReadPrice;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неверно введена цена");
+                        }
+
+                        Console.WriteLine("Введите id продукта");
+                        string inputId = Console.ReadLine();
+                        resultTryParse = double.TryParse(inputId, out var ReadId);
+                        if (resultTryParse == true)
+                        {
+                            item.price = ReadId;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неверно введена цена");
+                        }
+                        list.Add(item.id, item.name, item.price);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Введите id продукта для удаления");
+                        int n = int.Parse(Console.ReadLine());
+                        list.Delete(n);
+                        break;
+                    default:
+                        Console.WriteLine("Неверно выбрано действие");
+                        break;
+                }
+            }
         }
     }
 }
