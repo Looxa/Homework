b9e66c1b0a1404024e7c61be4b0eea4bb7ded197
Looxa <imxoloxas@gmail.com>
2022-01-21 18:44:13 +0300

    Добавил отдельную папку для классов

---
+++ ClassLibraryForHomework5/Class/Inventory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForHomework5.Class
+{
+
+    public class Inventory // Класс инвентаря 
+    {
+
+        List<Items> listOfItems = new List<Items>();
+        Items item1 = new Items();
+        public void Add(string name1, double price1) // Метод добавления продукта
+        {
+            Items itemX = new Items() { name = name1, price = price1 };
+            listOfItems.Add(itemX);
+        }
+        public void Delete(int DelNum1)  // Метод удаления продукта
+        {
+            // listOfItems.RemoveAt(listOfItems.IndexOf());
+            for (int i = 0; i < listOfItems.Count; i++)
+            {
+                if (DelNum1 == i)
+                {
+                    listOfItems.RemoveAt(i);
+                };
+            }
+
+
+
+        }
+        public void List()  //Метод вывода списка продуктов
+        {
+            double PriceCount = 0;
+            for (int i = 0; i < listOfItems.Count; i++)
+            {
+                listOfItems[i].id = i;
+                Console.WriteLine("Id: " + listOfItems[i].id + "\n Имя продукта: " + listOfItems[i].name + "\n Цена: " + listOfItems[i].price);
+                if (i < 1)
+                {
+                    PriceCount = listOfItems[i].price;
+                }
+                else
+                {
+                    PriceCount = listOfItems[i].price + PriceCount;
+                }
+            }
+            Console.WriteLine("Цена всего инвентаря: " + PriceCount);
+        }
+    }
+
+}
+
